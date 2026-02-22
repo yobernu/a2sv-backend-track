d79f9ac524ff0483b368723fba6c7fa056df725f
yobernu <yobernu@gmail.com>
2026-02-22 14:32:09 +0300

    created is palindrom file

--- task-2/main.go
+++ task-2/main.go
@@ -8,4 +8,10 @@ func main() {
 	test_input := "error reading go.mod: missing module declaration. To specify the module path:\n\n\tgo mod init <module_path>\n\nReplace <module_path> with the desired module path, which is typically the repository URL or a unique identifier for your project."
 	freq := word_frequency_count(test_input)
 	fmt.Println(freq)
+
+
+
+	// test case for palindrome
+	palindrome := is_palindrome("A man, a plan, a canal, Panama")
+	fmt.Println(palindrome)
 }
--- task-2/palindrome.go
+++ task-2/palindrome.go
@@ -1,1 +1,22 @@
 package main
+
+import "unicode"
+
+func is_palindrome(input string) bool {
+	var cleaned []rune
+	for _, r := range input {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			cleaned = append(cleaned, unicode.ToLower(r))
+		}
+	}
+
+	i, j := 0, len(cleaned)-1
+	for i < j {
+		if cleaned[i] != cleaned[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
